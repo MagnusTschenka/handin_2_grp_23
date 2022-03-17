fc6c51d633eff6302bf85dbb8d18975583bcf13e
DanielHolm999 <danielholm999@gmail.com>
2022-03-17 13:50:30 +0100

    More tests for Door

--- LadeskabUnitTest/TestDoor.cs
+++ LadeskabUnitTest/TestDoor.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using Ladeskab;
+using Ladeskab.Interfaces;
 
 namespace LadeskabUnitTest
 {
@@ -8,11 +9,17 @@ namespace LadeskabUnitTest
     public class Tests
     {
         private Door _uut;
+        private DoorChangedEventArgs _uutRecievedEventArgs;
 
         [SetUp]
         public void Setup()
         {
-            _uut = new Door(); 
+            _uutRecievedEventArgs = null;
+
+            _uut = new Door();
+            _uut.SetDoorStatus(true);
+
+            _uut.DoorChangedEvent += (o, args) => { _uutRecievedEventArgs = args; };
         }
 
         [Test]
@@ -21,23 +28,50 @@ namespace LadeskabUnitTest
             _uut.LockDoor();
             Assert.That(_uut.GetDoorLockedStatus(), Is.True);
         }
+        [Test]
         public void TestUnLockDoorFunction()
         {
             _uut.UnlockDoor();
             Assert.That(_uut.GetDoorLockedStatus(), Is.False);
         }
-
+        [Test]
         public void TestSimulateDoorBeingOpened()
         {
             _uut.simulateDoorBeingOpened();
             Assert.That(_uut.GetDoorStatus(), Is.True);
         }
+        [Test]
         public void TestSimulateDoorBeingClosed()
         {
             _uut.simulateDoorBeingClosed();
             Assert.That(_uut.GetDoorStatus(), Is.False);
         }
 
-        //mangler test for event???
+        //test af events uden Nsubstitute
+        [Test]
+        public void TestSetDoorStatusEvent_NotFired()
+        {
+            _uut.SetDoorStatus(true);
+            Assert.That(_uutRecievedEventArgs, Is.True);
+        }
+        [Test]
+        public void TestSetDoorStatusEvent_Fired()
+        {
+            _uut.SetDoorStatus(false);
+            Assert.That(_uutRecievedEventArgs, Is.False);
+        }
+        [Test]
+        public void TestSetDoorStatusEvent_CorrectDoorStatus()
+        {
+            _uut.SetDoorStatus(true);
+            Assert.That(_uutRecievedEventArgs.DoorStatus, Is.True);
+        }
+        [Test]
+        public void TestSetDoorStatusEvent_IncorretDoorStatus()
+        {
+            _uut.SetDoorStatus(true);
+            Assert.That(_uutRecievedEventArgs.DoorStatus, Is.False);
+        }
+        
     }
 }
