043be8ca36a2a35344892f86d039eb64bd3631d5
DanielHolm999 <danielholm999@gmail.com>
2022-03-17 12:35:59 +0100

    Display og IDisplay

--- Ladeskab/Interfaces/IDisplay.cs
+++ Ladeskab/Interfaces/IDisplay.cs
@@ -3,6 +3,12 @@ namespace Ladeskab.Interfaces
 {
     public interface IDisplay
     {
+        public void PrintConnectPhone();
+        public void PrintConnectionError();
+        public void PrintLoadRFID();
+        public void PrintRFIDError();
+        public void PrintOccupied();
+        public void PrintRemovedPhone();
     }
 }
 
--- Ladeskab/Models/Display.cs
+++ Ladeskab/Models/Display.cs
@@ -1,10 +1,44 @@
 ﻿using System;
+using Ladeskab.Interfaces;
+
 namespace Ladeskab
 {
-    public class Display
+    public class Display : IDisplay
     {
         public Display()
         {
+
+        }
+
+        public void PrintConnectionError()
+        {
+            Console.WriteLine("Tilslutningsfejl");
+        }
+
+        public void PrintConnectPhone()
+        {
+            Console.WriteLine("Tilsut telefon");
+        }
+
+        public void PrintLoadRFID()
+        {
+            Console.WriteLine("Indlæs RFID");
+
+        }
+
+        public void PrintOccupied()
+        {
+            Console.WriteLine("Ladeskab optaget");
+        }
+
+        public void PrintRemovedPhone()
+        {
+            Console.WriteLine("Fjern telefon");
+        }
+
+        public void PrintRFIDError()
+        {
+            Console.WriteLine("RFID fejl");
         }
     }
 }
